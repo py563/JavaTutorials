1fa8ab40f9216a73e8092b83385e5bd963ee1adb
py563 <maredukondaprajwal@gmail.com>
2019-03-27 16:39:33 -0400

    method overlaoding commit

--- JavaConcepts.java
+++ JavaConcepts.java
@@ -1,12 +1,14 @@
 package JavaTutorials;
+
+import java.util.ArrayList;
+
+// Denonstrates OOPS concepts
 // person class - super class 
 class Person {
+    //common variables
     private String name;
     private String address;
-    public Person(String name, String address){
-        this.name = name;
-        this.address = address;
-    }
+    
     /**
      * @param address the address to set
      */
@@ -31,16 +33,19 @@ class Person {
     public String getName() {
         return name;
     }
+    public void schedule(String[] days){
+        System.out.println("Person class schedule doesn't call this method when using subclass");
+    }
 
 }
 // student class
 class Student extends Person{
+    //student class variables
     private int id;
     private String college;
-    public Student(int id, String college, String name, String address) {
-        super(name, address);
-        this.id = id;
-        this.college = college;
+    //constructors
+    public Student() {
+        super();
     }
     /**
      * @param college the college to set
@@ -66,24 +71,93 @@ class Student extends Person{
     public int getId() {
         return id;
     }
-// schedule method
- public void schedule(String[] days){
-     System.out.println(getName() +" has Classes on");
-     for (String day : days)
-        System.out.print(day+", ");
+    // schedule method
+    @Override
+    public void schedule(String[] days){
+        System.out.print(getName() +" has Classes on");
+        for (String day : days)
+            System.out.print(", "+day);
+        System.out.println(".");
  }
     
+}
+// employee class
+class Employee extends Person{
+    // employee specific variables
+    private int employeeID;
+    private String company;
+    // constructors
+    public Employee(){
+        super();
+    }
+    /**
+     * @param company the company to set
+     */
+    public void setCompany(String company) {
+        this.company = company;
+    }
+    /**
+     * @param employeeID the employeeID to set
+     */
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+    /**
+     * @return the employeeID
+     */
+    public int getEmployeeID() {
+        return employeeID;
+    }
+    /**
+     * @return the company
+     */
+    public String getCompany() {
+        return company;
+    }
+    // schedule method
+    @Override
+    public void schedule(String[] days){
+        System.out.print(getName() +" goes to Work on");
+        for (String day : days)
+            System.out.print(", "+ day);
+        System.out.println(".");
+ }
 }
 // main class
 public class JavaConcepts {
  public static void main(String[] args) {
-     Person tom = new Person("tom", "55 Gage Ave");
-     tom.setName("thomas");
-     System.out.println(tom.getName());
-     Student tim = new Student(11, "sheridan", "tim", "66 aaa");
-     String[] days = {"Monday", "Friday"};
+    String[] days = {"Monday", "Friday"};  
+    //Student object
+     Student tom = new Student();
+     tom.setName("tom");
+     tom.setAddress("66 aaa");
+     tom.setId(11);
+     tom.setCollege("sheridan");
+     tom.schedule(days);
+     //Employee Object
+     Employee jim = new Employee();
+     jim.setName("Jim");
+     jim.setAddress("66 aaa");
+     jim.setEmployeeID(11);
+     jim.setCompany("sheridan");
+     jim.schedule(days);
+
+     //Student object through Person class
+     Person tim = new Person();
+     tim.setName("tim");
+     tim.setAddress("66 aaa");
+     // will throw an error 
+     //tim.setId(11);
      tim.schedule(days);
-     tim.setAddress("65 aaa");
-     tim.setId(12);
+
+     ArrayList<Person> al = new ArrayList<Person>();
+     al.add(tom);
+     al.add(jim);
+     al.add(tim);
+     for ( Person a : al){
+        System.out.println(a.getClass());
+     }
+     
+
  }
 }
